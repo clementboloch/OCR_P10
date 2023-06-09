017a3f6fda4ed277228e80bc71377f5b9ebea92b
clementboloch <clement.boloch@gmail.com>
2023-06-09 12:41:56 +0200

    creates models

--- src/issueTracking/admin.py
+++ src/issueTracking/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+admin.site.register(models.Project)
+admin.site.register(models.Contributor)
+admin.site.register(models.Issue)
+admin.site.register(models.Comment)
--- src/issueTracking/models.py
+++ src/issueTracking/models.py
@@ -1,3 +1,64 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=250)
+    type = [
+        ("BE", "Backe-end"),
+        ("FE", "Front-end"),
+        ("iOS", "iOS"),
+        ("Andr", "Android"),
+    ]
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
+class Contributor(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    # permission
+    # role
+
+    def __str__(self):
+        return f"{self.user} - {self.project}"
+
+
+class Issue(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=250)
+    tag = [
+        ("BUG", "Bug"),
+        ("IMPROVEMENT", "Amélioration"),
+        ("TASK", "Tâche"),
+    ]
+    priority = [
+        ("LOW", "Faible"),
+        ("MEDIUM", "Moyenne"),
+        ("HIGH", "Élevée"),
+    ]
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    status = [
+        ("TODO", "À faire"),
+        ("IN_PROGRESS", "En cours"),
+        ("DONE", "Terminé"),
+    ]
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    assignee = models.ForeignKey(User, on_delete=models.CASCADE, related_name="assignee", default=author)
+    created_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.title} - {self.project} ({self.status})"
+
+
+class Comment(models.Model):
+    description = models.CharField(max_length=250)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    issue = models.ForeignKey(Issue, on_delete=models.CASCADE)
+    created_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.issue} - {self.description[:20]}"
