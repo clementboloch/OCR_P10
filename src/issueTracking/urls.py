1bcef13f2b9c5e69e3d3291090ac7eda6b2eda1b
clementboloch <clement.boloch@gmail.com>
2023-06-23 12:53:40 +0200

    adds a delete contributor view

--- src/issueTracking/urls.py
+++ src/issueTracking/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.ProjectList.as_view(), name='project_list'),
     path('<int:pk>/', views.ProjectDetail.as_view(), name='project_detail'),
     path('<int:pk>/users/', views.UserList.as_view(), name='contributor_list'),
+    path('<int:project_id>/users/<int:user_id>/', views.ContributorDelete.as_view(), name='contributor_delete'),
 ]
 
 # /signup/
--- src/issueTracking/views.py
+++ src/issueTracking/views.py
@@ -1,5 +1,6 @@
 # from itertools import chain
 from itertools import chain
+from django.shortcuts import get_object_or_404
 from rest_framework import generics
 from . import models
 from . import serializers
@@ -31,3 +32,14 @@ class UserList(generics.ListCreateAPIView):
         project_id = self.kwargs['pk']
         project = models.Project.objects.get(id=project_id)
         serializer.save(project=project)
+
+
+class ContributorDelete(generics.DestroyAPIView):
+    queryset = models.Contributor.objects.all()
+    serializer_class = serializers.ContributorSerializer
+
+    def get_object(self, queryset=None):
+        project_id = self.kwargs.get('project_id')
+        user_id = self.kwargs.get('user_id')
+        contributor = get_object_or_404(self.queryset, project_id=project_id, user_id=user_id)
+        return contributor
