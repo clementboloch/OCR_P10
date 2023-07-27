2d24a292dd3fef1393a3a1dbaf6981fb2c8a2e67
clementboloch <clement.boloch@gmail.com>
2023-07-27 15:48:08 +0200

    adds a IsAuthor permission class

--- src/issueTracking/permissions.py
+++ src/issueTracking/permissions.py
@@ -11,3 +11,14 @@ class IsContributor(permissions.BasePermission):
         # Only allow the contributor of the project to access it.
         # Check if a Contributor instance exists with the user as user and the project as project.
         return Contributor.objects.filter(user=request.user, project=obj).exists()
+
+
+class IsAuthor(permissions.BasePermission):
+    """
+    Custom permission to only allow authors of a ressource to access it.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        # Only allow the author of the ressource to access it.
+        # Check if the author of the ressource is the user.
+        return obj.author == request.user
