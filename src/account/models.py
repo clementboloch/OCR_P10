62df43f3d144bb71a1962da79d38cffcd9c1ae15
clementboloch <clement.boloch@gmail.com>
2023-08-11 17:18:37 +0200

    updates user registration

--- src/account/models.py
+++ src/account/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager
+from datetime import date
 
 
 class UserManager(BaseUserManager):
@@ -14,7 +15,6 @@ class UserManager(BaseUserManager):
         return user
 
     def create_superuser(self, email, password, **extra_fields):
-        extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
         extra_fields.setdefault("is_active", True)
 
@@ -28,19 +28,20 @@ class UserManager(BaseUserManager):
 
 class UserData(AbstractUser):
     username = None
-    first_name = models.CharField(max_length=100, unique=True)
-    last_name = models.CharField(max_length=100, unique=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
     email = models.EmailField(max_length=100, unique=True)
+    birthdate = models.DateField(default=date.today)
+    can_be_contacted = models.BooleanField(default=False)
+    can_data_be_shared = models.BooleanField(default=False)
     date_joined = models.DateTimeField(auto_now_add=True)
-    is_admin = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
 
     objects = UserManager()
 
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = ["first_name", "last_name"]
+    REQUIRED_FIELDS = ["first_name", "last_name", "birthdate", "can_be_contacted", "can_data_be_shared"]
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
--- src/account/serializers.py
+++ src/account/serializers.py
@@ -5,7 +5,7 @@ from .models import UserData
 class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserData
-        fields = ["id", "email", "first_name", "last_name", "password"]
+        fields = ["id", "email", "first_name", "last_name", "password", "birthdate", "can_be_contacted", "can_data_be_shared"]
         extra_kwargs = {
             'password': {'write_only': True},
         }
@@ -15,6 +15,9 @@ class RegisterSerializer(serializers.ModelSerializer):
             email=validated_data["email"],
             first_name=validated_data["first_name"],
             last_name=validated_data["last_name"],
+            birthdate=validated_data["birthdate"],
+            can_be_contacted=validated_data["can_be_contacted"],
+            can_data_be_shared=validated_data["can_data_be_shared"]
         )
         user.set_password(validated_data["password"])
         user.save()
