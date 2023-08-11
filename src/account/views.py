45e3d7fcde1635f9ba0ef2fa9acaa99651dc6f6e
clementboloch <clement.boloch@gmail.com>
2023-08-11 17:38:02 +0200

    sets an age limit for data sharing

--- src/account/views.py
+++ src/account/views.py
@@ -2,11 +2,24 @@ from rest_framework.views import APIView
 from .serializers import RegisterSerializer
 from rest_framework.response import Response
 
+from datetime import date
+
 
 # view for registering users
 class RegisterView(APIView):
     def post(self, request):
-        serializer = RegisterSerializer(data=request.data)
+        data = request.data
+        data._mutable = True
+        birthdate = data.get('birthdate')
+        birthdate = date.fromisoformat(birthdate)
+        today = date.today()
+        age = today.year - birthdate.year - ((today.month, today.day) < (birthdate.month, birthdate.day))
+
+        if age < 15:
+            data['can_be_contacted'] = False
+            data['can_data_be_shared'] = False
+
+        serializer = RegisterSerializer(data=data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
